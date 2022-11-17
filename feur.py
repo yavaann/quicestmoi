c903f6bb547654f028335b15d9e159d88c768b6a
Yavan <yavan.parien@gmail.com>
2022-11-17 15:14:18 +0100

    Début code

---
+++ feur.py
@@ -0,0 +1,23 @@
+from p5 import *
+from random import randint
+from PIL import Image
+from random import randint
+
+
+
+
+
+def setup():
+    size(980,720)
+
+
+
+def draw():
+    x = randint(6, 594)
+    y = randint(6, 294)
+    diametre = 10
+    fill(0, 255, 0)
+    circle(x, y, diametre)
+    image(load_image("Al.png"), 100,100,100,100)
+    
+run()
