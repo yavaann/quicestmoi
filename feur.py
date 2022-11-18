e1c0c82e0d2205c01395c1030b79098fdf3b641c
Yavan <yavan.parien@gmail.com>
2022-11-18 21:15:11 +0100

    tout les fichiez 2

--- feur.py
+++ feur.py
@@ -1,21 +1,49 @@
 from p5 import *
 from random import randint
 from PIL import Image
-from random import randint
+import sqlite3
+bdd = sqlite3.connect("bdd/animalcrossing.db")
+curseur = bdd.cursor()
+req = "SELECT perso_name FROM perso WHERE perso_id ="+str()
+
+
+image_perso_liste = []
+x=50
+y=-50
+compteur_id_perso = 1
+font = create_font("police/Aqum2.otf",34)
+text_font(font, 34)
+
+class image_personnage():
+    def __init__(self,x,y,perso_id):
+        self.x = x
+        self.y = y
+        req = "SELECT perso_name FROM perso WHERE perso_id ="+str(perso_id)
+        r = curseur.execute(req)
+        self.nom_perso = r.fetchall()[0][0]
+
+    def afficher(self):
+        image(load_image("images/"+self.nom_perso+".png"),self.x,self.y,50,50)
+        text_align(align_x="CENTER")
+        fill(0,0,0)
+        text(str(self.nom_perso),self.x+25,self.y+50)
+
+for i in range(1,6):
+    y+=100
+    x=50
+    for j in range(1,6):
+        compteur_id_perso+=1
+        image_perso_liste.append(image_personnage(x,y,compteur_id_perso))
+        x +=100
+
 
 
 
 def setup():
-    size(1200,600)
-
-
-
+    size(1200,560)
+    for image_perso in image_perso_liste:
+        image_perso.afficher()
 def draw():
-    x = randint(6, 594)
-    y = randint(6, 294)
-    diametre = 10
-    fill(0, 255, 0)
-    circle(x, y, diametre)
-    image(load_image("Al.png"), 100,100,100,100)
+    circle(20,20,20)
     
 run()
