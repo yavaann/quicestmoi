ed2bf0628078d31d17f774c9525bd33ed3c6c821
fire <lauxm.marc@gmail.com>
2023-01-20 23:10:30 +0100

    jsp ce que je fais

---
+++ comparaison perso (test).py
@@ -0,0 +1,15 @@
+caracteristiques=["Visage","Cheuveux","Yeux","Bouche","Nez","Bacground","Genre","Cosmetique"]
+yeux=["Rouge","Vert","Noir","Bleu","Violet"]
+cheuveux=["Marron","Vert","Jaune","Bleu","Chauve"]
+visage=["Blanc","Noir"]
+nez=["Gros","Petit"]
+background=["Espace","Foret","Ville"]
+bouche=["Ouvert","Ferme","Pilosite","Sourire"]
+cosmetique=["Rougissement","TacheDeRousseur"]
+
+
+
+def comparaison(choisi):
+    
+   
+    
